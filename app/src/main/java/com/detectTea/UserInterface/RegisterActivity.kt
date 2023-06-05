491115dca1f408c6243c5d9b43a9698d444c5be2
Andripardosi <saoguru00@gmail.com>
2023-06-05 19:55:39 +0700

    Mengunggah file proyek

---
+++ app/src/main/java/com/detectTea/UserInterface/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.detectTea.UserInterface
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.detectTea.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
